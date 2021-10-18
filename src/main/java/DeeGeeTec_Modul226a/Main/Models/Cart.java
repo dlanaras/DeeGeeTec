1f7450f57caac27f352fa041db87ac0bbd58edd8
ItzSveniSchule <sven.oppliger@edu.tbz.ch>
2021-10-18 10:05:45 +0200

    Updated Classes
    Added function addItemToWishlist in class Wishlist
    
    Added function addItemToCart in class Cart

--- src/main/java/DeeGeeTec_Modul226a/Main/Models/Cart.java
+++ src/main/java/DeeGeeTec_Modul226a/Main/Models/Cart.java
@@ -3,14 +3,18 @@ package DeeGeeTec_Modul226a.Main.Models;
 import java.util.List;
 
 public class Cart {
-    private List<Item> items;
+    private List<Item> cartItems;
 
     public List<Item> getItems() {
-        return items;
+        return cartItems;
     }
 
     public void setItems(List<Item> items) {
-        this.items = items;
+        this.cartItems = items;
     }
 
+    public void addItem(Item itemToAdd){
+        //adds am Item to the Cart
+        cartItems.add(itemToAdd);
+    };
 }
--- src/main/java/DeeGeeTec_Modul226a/Main/Models/Wishlist.java
+++ src/main/java/DeeGeeTec_Modul226a/Main/Models/Wishlist.java
@@ -3,13 +3,18 @@ package DeeGeeTec_Modul226a.Main.Models;
 import java.util.List;
 
 public class Wishlist {
-    private List<Item> items;
+    private List<Item> wishlistItems;
 
     public List<Item> getItems() {
-        return items;
+        return wishlistItems;
     }
 
     public void setItems(List<Item> items) {
-        this.items = items;
+        this.wishlistItems = items;
+    }
+
+    public void addItemToWishlist(Item itemToAdd){
+        //adds an item to the wishlist
+        wishlistItems.add(itemToAdd);
     }
 }
