be9cae3bbecf8e0f9b3ccc39cfea8c23a61befc0
dlanaras <dimitrislanaras04@outlook.com>
2021-11-04 09:48:32 +0100

    Added more tests

--- src/test/java/DeeGeeTec_Modul226a/Main/Tests/AccountHandlerTest.java
+++ src/test/java/DeeGeeTec_Modul226a/Main/Tests/AccountHandlerTest.java
@@ -3,7 +3,14 @@ package DeeGeeTec_Modul226a.Main.Tests;
 import DeeGeeTec_Modul226a.Main.Controllers.AccountHandler;
 import DeeGeeTec_Modul226a.Main.Models.Account;
 import static org.junit.jupiter.api.Assertions.*;
+
+
+import jdk.jshell.spi.ExecutionControl;
+import org.apache.commons.lang3.NotImplementedException;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
+
+import java.net.HttpURLConnection;
 
 public class AccountHandlerTest {
     private final AccountHandler accountHandler;
@@ -12,34 +19,37 @@ public class AccountHandlerTest {
         accountHandler = new AccountHandler();
     }
 
-    //TODO: they should return an httpstatus that i can use to compare
-    /*@Test
+    @Test
     public void UserCanLogin() {
-        assertEquals(accountHandler.Login("Joe", "HashedPassword"), HttpURLConnection.HTTP_OK) //or something like that
+        assertEquals(accountHandler.Login("Joe", "HashedPassword").getStatusCode(), HttpStatus.OK);
     }
 
     @Test
     public void UserCantLoginWithWrongUsername() {
-        assertNotEquals(accountHandler.Login("Joee", "HashedPassword").equals(HttpURLConnection.HTTP_OK)) //or something like that
+        throw new NotImplementedException();
+        //assertNotEquals(accountHandler.Login("Joee", "HashedPassword"),HttpURLConnection.HTTP_OK);
 
     }
 
     @Test
     public void UserCantLoginWithWrongPassword() {
-        assertNotEquals(accountHandler.Login("Joe", "HashedPassworde").equals(HttpURLConnection.HTTP_OK)) //or something like that
+        throw new NotImplementedException();
+        //assertNotEquals(accountHandler.Login("Joe", "HashedPassworde").equals(HttpURLConnection.HTTP_OK))
 
     }
 
     @Test
     public void UserCanRegister() {
-        assertEquals(accountHandler.Register("Joe", "HashedPassword", ...), HttpURLConnection.HTTP_CREATED)
-    }*/
+        throw new NotImplementedException();
+        //assertEquals(accountHandler.Register("Joe", "HashedPassword", ...), HttpURLConnection.HTTP_CREATED)
+    }
 
     /**
      * Test to check if logging in is possible
      */
     @Test
     public void ReturnsCurrentUser(){
+        throw new NotImplementedException();
         /*
         accountHandler.Login("Joe", "HashedPassword");
         string userName = accountHandler.GetCurrentUser().getUsername()
---
+++ src/test/java/DeeGeeTec_Modul226a/Main/Tests/OrderHandlerTest.java
@@ -0,0 +1,22 @@
+package DeeGeeTec_Modul226a.Main.Tests;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.junit.Test;
+
+public class OrderHandlerTest {
+
+    @Test
+    public void OrderGetsCreatedWhenUserPurchasesSomething() {
+        throw new NotImplementedException();
+    }
+
+    @Test
+    public void ReturnsAllOrdersOfPerson() {
+        throw new NotImplementedException();
+    }
+
+    @Test
+    public void ReturnsSpecificOrderOfPerson() {
+        throw new NotImplementedException();
+    }
+}
