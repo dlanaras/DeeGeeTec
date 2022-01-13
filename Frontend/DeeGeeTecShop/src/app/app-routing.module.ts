19115d9f4f5ebccf498041520692bc6d73b85db6
dlanaras <dimitrislanaras04@outlook.com>
2022-01-13 10:51:09 +0100

    added angular-material and some routing

--- Frontend/DeeGeeTecShop/src/app/app-routing.module.ts
+++ Frontend/DeeGeeTecShop/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FourohfourComponent } from './fourohfour/fourohfour.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "mainpage", pathMatch: "full"},
+  { path: "mainpage", component: MainComponent},
+  { path: "**", component: FourohfourComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/DeeGeeTecShop/src/app/app.module.ts
+++ Frontend/DeeGeeTecShop/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { FourohfourComponent } from './fourohfour/fourohfour.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from "@angular/material/slider";
+import { MatProgressBarModule } from "@angular/material/progress-bar"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FourohfourComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
